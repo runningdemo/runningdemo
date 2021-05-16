942d3a7cfaf03183caa753c9a65aeb7587685501
Demo <kidliaa@gmail.com>
2021-05-16 12:34:19 +0800

    publish

--- src/publish.sh
+++ src/publish.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$BASH_SOURCE")"
-sh ./build.sh
-git add -A
-git commit -m "publish"
-git push
+# sh ./build.sh
+if [ -z $(git status --porcelain) ]; then
+  echo 'not changed'
+else
+  echo 'changed'
+  git add -A
+  git commit -m "publish"
+fi
+
+# git push
